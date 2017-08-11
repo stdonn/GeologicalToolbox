d2e8b3a087473ce92cc35e4ba3331eb33b93493c
Stephan Donndorf <stephan@donndorf.info>
2017-08-11 10:07:14 +0200

    created DBHandler.py with basic SQLAlchemy functionality.

---
+++ DBHandler.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sq
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class DBHandler:
+	def __init__(self, connection='sqlite:///:memory:', debug=False):
+		engine = sq.create_engine(connection, echo=debug)
+		Base.metadata.create_all(engine)
+		self.__Session = sessionmaker(bind=engine)
+
+	def getSession(self):
+		return self.__Session()
