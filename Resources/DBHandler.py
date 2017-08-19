b9fbc98f0f095f6f8bedbcb3fb466a4f24b317dc
Stephan Donndorf <stephan@donndorf.info>
2017-08-19 14:38:38 +0200

    Added return type and docstring for testing purposes

--- Resources/DBHandler.py
+++ Resources/DBHandler.py
@@ -9,9 +9,15 @@ Base = declarative_base()
 
 class DBHandler:
 	def __init__(self, connection='sqlite:///:memory:', debug=False):
+		# type: (str, bool) -> None
+		"""
+		:rtype: None
+
+		"""
 		engine = sq.create_engine(connection, echo=debug)
 		Base.metadata.create_all(engine)
 		self.__Session = sessionmaker(bind=engine)
 
 	def get_session(self):
+		# type: () -> sessionmaker
 		return self.__Session()
