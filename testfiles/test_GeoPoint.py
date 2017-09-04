de503351dbbe677d71ba8442636406b132648673
Stephan Donndorf <stephan@donndorf.info>
2017-09-04 11:31:59 +0200

    Integrated unique point constraint and -1 standart for line_id and horizon_id

--- testfiles/test_GeoPoint.py
+++ testfiles/test_GeoPoint.py
@@ -7,33 +7,36 @@ from Resources.Geometries import GeoPoint
 from Resources.Stratigraphy import Stratigraphy
 
 if __name__ == '__main__':
-	handler = DBHandler(connection='sqlite:///D:\\data.db', debug=False)
+	handler = DBHandler(
+			connection='sqlite:////Users/stephan/Documents/work/Dissertation/GIS/Modelling-Toolbox/data.db',
+			debug=False)
+	# handler = DBHandler(connection='sqlite:///D:\\data.db', debug=False)
+	session = handler.get_session()
 
 	points = [
 		{
 			'east' : 1234567.12,
 			'north': 3124235.23,
 			'alt': 132.32,
-			'horizon' : 'mu',
-			'age'     : 2
+			'horizon' : 'mo',
+			'age'     : 4
 		}, {
 			'east' : 2342432.34,
 			'north': 2342317.65,
 			'alt': 342.12,
-			'horizon' : 'so',
-			'age'     : 1
+			'horizon' : 'su',
+			'age'     : 5
 		}, {
 			'east' : 3234123.98,
 			'north': 7654395.34,
 			'alt': 543.34,
-			'horizon' : 'mm',
-			'age'     : 3
+			'horizon' : 'ku',
+			'age'     : 6
 		}
 	]
 
 	Points = [GeoPoint(easting=point['east'], northing=point['north'], altitude=point['alt'],
-	                   horizon=Stratigraphy(point['horizon'], point['age'])) for point in points]
-	session = handler.get_session()
+	                   horizon=Stratigraphy(point['horizon'], point['age']), session=session) for point in points]
 	session.add_all(Points)
 
 	try:
