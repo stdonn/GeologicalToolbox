97e0c2e171a9e28a12ecd20ce30d1a3181d26f96
Stephan Donndorf <stephan@donndorf.info>
2017-09-21 22:22:16 +0200

    1) Added a position value in GeoPoint table to store the point index in a line or -1 2) Added name columns, setters and getters for GeoPoint and Line tables to create a "feature like" behavior 3) Added a has_z-Value to GeoPoint to differ between z and no data values 4) Added a delete_z-function to GeoPoint 5) Added a load_by_id_from_db function to line 6) function load_in_extent_form_db(...) works now

--- Resources/Geometries.py
+++ Resources/Geometries.py
@@ -2,6 +2,7 @@
 
 import sqlalchemy as sq
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.orderinglist import ordering_list
 from sqlalchemy.orm import relationship
 
 from Exceptions import DatabaseException
@@ -16,27 +17,37 @@ class GeoPoint(Base):
 	east = sq.Column(sq.REAL(10, 4))
 	north = sq.Column(sq.REAL(10, 4))
 	alt = sq.Column(sq.REAL(10, 4))
+	has_z = sq.Column(sq.BOOLEAN, default=False)
 	horizon_id = sq.Column(sq.INTEGER, sq.ForeignKey('stratigraphy.id'), default=-1)
 	hor = relationship("Stratigraphy")
 
 	line_id = sq.Column(sq.INTEGER, sq.ForeignKey('lines.id'), default=-1)
+	line_pos = sq.Column(sq.INTEGER, default=-1)
+	name = sq.Column(sq.TEXT(100), default="")
 
 	# make the points unique -> coordinates + horizon + belongs to one line?
-	sq.UniqueConstraint(east, north, alt, horizon_id, line_id, name='u_point_constraint')
+	sq.UniqueConstraint(east, north, alt, horizon_id, line_id, line_pos, name='u_point_constraint')
 
-	def __init__(self, easting, northing, altitude, horizon, session):
+	def __init__(self, easting, northing, altitude, horizon, session, name=""):
 		self.easting = easting
 		self.northing = northing
-		self.altitude = altitude
+		if altitude is None:
+			self.altitude = 0
+			self.has_z = False
+		else:
+			self.altitude = altitude
+			self.has_z = True
 		self.__session = session
 		self.horizon = horizon
+		self.point_name = name
 
 	def __repr__(self):
-		return "<GeoPoint(id='{}', east='{}', north='{}', alt='{}', horizon='{}')>" \
-			.format(self.id, self.easting, self.northing, self.altitude, str(self.horizon))
+		return "<GeoPoint(id='{}', east='{}', north='{}', alt='{}', horizon='{}', line={}, line-position={})>" \
+			.format(self.id, self.easting, self.northing, self.altitude, str(self.horizon), self.line_id, self.line_pos)
 
 	def __str__(self):
-		return "[{}] {} - {} - {} : {}".format(self.id, self.easting, self.northing, self.altitude, str(self.horizon))
+		return "[{}] {} - {} - {} : {} - {} - {}" \
+			.format(self.id, self.easting, self.northing, self.altitude, str(self.horizon), self.line_id, self.line_pos)
 
 	@property
 	def easting(self):
@@ -61,6 +72,7 @@ class GeoPoint(Base):
 	@altitude.setter
 	def altitude(self, value):
 		self.alt = value
+		self.has_z = True
 
 	@property
 	def horizon(self):
@@ -83,6 +95,10 @@ class GeoPoint(Base):
 			raise DatabaseException('More than one ({}) horizon with the same name: {}! Database error!'
 			                        .format(result.count(), value.name))
 
+	def del_z(self):
+		self.alt = 0
+		self.has_z = False
+
 	def save_to_db(self):
 		self.__session.add(self)
 		try:
@@ -92,6 +108,16 @@ class GeoPoint(Base):
 			raise IntegrityError(
 					'Cannot commit changes in geopoints table, Integrity Error (double unique values?) -- {} -- Rolling back changes...'.format(
 							e.statement), e.params, e.orig, e.connection_invalidated)
+
+	@property
+	def point_name(self):
+		return self.name
+
+	@point_name.setter
+	def point_name(self, value):
+		if len(value) > 100:
+			value = value[:100]
+		self.name = value
 
 	@classmethod
 	def load_all_from_db(cls, session):
@@ -108,6 +134,7 @@ class Line(Base):
 
 	id = sq.Column(sq.INTEGER, sq.Sequence('line_id_seq'), primary_key=True)
 	closed = sq.Column(sq.BOOLEAN)
+	name = sq.Column(sq.TEXT(100), default="")
 
 	horizon_id = sq.Column(sq.INTEGER, sq.ForeignKey('stratigraphy.id'))
 	hor = relationship("Stratigraphy")
@@ -115,22 +142,24 @@ class Line(Base):
 	# @AssociationType GeoPoint
 	# @AssociationMultiplicity 2..*
 
-	points = relationship("GeoPoint", order_by=GeoPoint.id, backref="line", primaryjoin='Line.id==GeoPoint.line_id',
+	points = relationship("GeoPoint", order_by=GeoPoint.line_pos, collection_class=ordering_list('line_pos'),
+	                      backref="line", primaryjoin='Line.id==GeoPoint.line_id',
 	                      cascade="all, delete, delete-orphan")
 
-	def __init__(self, closed, session, horizon, points):
+	def __init__(self, closed, session, horizon, points, name=""):
 		self.is_closed = closed
 		self.__session = session
 		self.horizon = horizon
 		self.points = points
-		self.__last_error_message = ''
+		self.line_name = name
+		self.__last_error_message = ""
 
 	def __repr__(self):
 		return "<Line(id='{}', closed='{}', horizon='{}'\npoints='{}')>" \
 			.format(self.id, self.closed, str(self.horizon), str(self.points))
 
 	def __str__(self):
-		#return "[{}] {} - {}\n{}".format(self.id, self.closed, str(self.horizon), str(self.points))
+		# return "[{}] {} - {}\n{}".format(self.id, self.closed, str(self.horizon), str(self.points))
 		text = "Line: id='{}', closed='{}', horizon='{}', points:" \
 			.format(self.id, self.closed, str(self.horizon))
 
@@ -173,6 +202,16 @@ class Line(Base):
 		else:
 			raise DatabaseException('More than one ({}) horizon with the same name: {}! Database error!'
 			                        .format(result.count(), value.name))
+
+	@property
+	def line_name(self):
+		return self.name
+
+	@line_name.setter
+	def line_name(self, value):
+		if len(value) > 100:
+			value = value[:100]
+		self.name = value
 
 	def insert_point(self, point, position):
 		"""@ParamType point GeoPoint
@@ -254,18 +293,35 @@ class Line(Base):
 			self.__session.commit()
 		except IntegrityError as e:
 			self.__session.rollback()
-			raise IntegrityError(
-					'Cannot commit changes in lines table, Integrity Error (double unique values?) -- {} -- Rolling back changes...'.format(
-							e.statement), e.params, e.orig, e.connection_invalidated)
+			self.__last_error_message = 'Cannot commit changes in lines table, Integrity Error (double unique values?) -- {} -- Rolling back changes...'.format(
+					e.statement)
+			raise IntegrityError(self.__last_error_message, e.params, e.orig, e.connection_invalidated)
 
 	@classmethod
 	def load_all_from_db(cls, session):
 		return session.query(cls).all()
 
 	@classmethod
-	def load_in_extend_from_db(cls, session, min_easting, max_easting, min_northing, max_northing):
-		return session.query(cls).filter(min_easting <= cls.easting, cls.easting <= max_easting,
-		                                 min_northing <= cls.northing, cls.northing <= max_northing).all()
+	def load_by_id_from_db(cls, id, session):
+		return session.query(cls).filter(cls.id == id).one()
+
+	@classmethod
+	def load_in_extent_from_db(cls, session, min_easting, max_easting, min_northing, max_northing):
+
+		# select the points, result will be a list of tuples with only an line-id value
+		# -> convert to list and the to a set to remove doubled values
+		points = set(
+				[x[0] for x in session.query(GeoPoint.line_id). \
+					filter(GeoPoint.line_id != -1). \
+					filter(sq.between(GeoPoint.east, min_easting, max_easting)). \
+					filter(sq.between(GeoPoint.north, min_northing, max_northing)).all()
+				 ])
+
+		# return line with points in extend
+		return session.query(Line).filter(Line.id.in_(points)).all()
+
+	# return session.query(cls).filter(min_easting <= cls.easting, cls.easting <= max_easting,
+	#                                min_northing <= cls.northing, cls.northing <= max_northing).all()
 
 	def last_error_message(self):
 		"""@ReturnType string"""
