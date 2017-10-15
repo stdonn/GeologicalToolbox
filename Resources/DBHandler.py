f801fcd64349e82cb950fc71a5ca9f05c384150d
Stephan Donndorf <stephan@donndorf.info>
2017-10-15 19:12:08 +0200

    - added docstrings

--- Resources/DBHandler.py
+++ Resources/DBHandler.py
@@ -1,4 +1,7 @@
 # -*- coding: UTF-8 -*-
+"""
+This module provides a class for database access through an SQLAlchemy session.
+"""
 
 import sqlalchemy as sq
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,11 +11,21 @@ Base = declarative_base()
 
 
 class DBHandler:
+	"""
+	A class for database access through an SQLAlchemy session.
+	"""
 	def __init__(self, connection='sqlite:///:memory:', debug=False):
 		# type: (str, bool) -> None
 		"""
-		:rtype: None
+		Initialize a new database connection via SQLAlchemy
 
+		:param connection: Connection uri to a database, format defined by SQLAlchemy
+		:type connection: str
+
+		:param debug: enable debug output for database access (default False)
+		:type debug: bool
+
+		:return: Nothing
 		"""
 		engine = sq.create_engine(connection, echo=debug)
 		Base.metadata.create_all(engine)
@@ -20,4 +33,9 @@ class DBHandler:
 
 	def get_session(self):
 		# type: () -> sessionmaker
+		"""
+		Returns the a session for the current database connection
+
+		:return: Returns the a session for the current database connection
+		"""
 		return self.__Session()
