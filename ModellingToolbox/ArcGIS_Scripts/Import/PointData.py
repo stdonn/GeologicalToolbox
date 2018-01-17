1c77650ca66a1e5725b60c09ba816bb568760346
Stephan Donndorf <stephan.donndorf@googlemail.com>
2018-01-17 22:53:35 +0100

    Added import points from file function

--- ModellingToolbox/ArcGIS_Scripts/Import/PointData.py
+++ ModellingToolbox/ArcGIS_Scripts/Import/PointData.py
@@ -6,6 +6,8 @@ A module controlling the import of point data into a database.
 
 from ModellingToolbox.Exceptions import ArgumentError
 from ModellingToolbox.Resources.DBHandler import DBHandler
+from ModellingToolbox.Resources.Geometries import GeoPoint
+from ModellingToolbox.Resources.Stratigraphy import StratigraphicObject
 
 from sqlalchemy.orm.session import Session
 
@@ -18,6 +20,7 @@ class PointImport:
     """
     the base class for importing point data
     """
+
     def __init__(self, sessionobject):
         # type: (Session) -> None
         """
@@ -38,8 +41,51 @@ class PointImport:
         :return: nothing
         :raises ArgumentError: if the number of arguments is wrong
         """
-        if len(args) < 5:
-            raise ArgumentError('Too less arguments ({}). Should be {}.'.format(len(args), 5))
+        if len(args) < 9:
+            arguments = "data file path and following column numbers (starting with 1):\n"
+            arguments += "easting, northing, altitude, stratigraphy, stratigraphic age, point set name, comment\n"
+            arguments += "column separator inside the data file"
+            raise ArgumentError('Missing arguments. Should be:\n{}'.format(arguments))
+
+        point_data = args[0]
+        separator = args[8]
+        columns = {
+            'easting'   : int(args[1]) - 1,
+            'northing'  : int(args[2]) - 1,
+            'altitude'  : int(args[3]) - 1,
+            'strat'     : int(args[4]) - 1,
+            'age'       : int(args[5]) - 1,
+            'points_set': int(args[6]) - 1,
+            'comment'   : int(args[7]) - 1
+        }
+
+        for key in columns:
+            if columns[key] < -1:
+                columns[key] = 1
+
+        if -1 in (columns['easting'], columns['northing']):
+            raise ArgumentError('You have to specify a column for easting, northing and altitude values!')
+
+        point_file = open(point_data, "r")
+
+        for line in point_file:
+            line = line.split(separator)
+
+            result = dict()
+            for col in columns:
+                if col != -1:
+                    result[col] = line[columns[col]]
+                else:
+                    result[col] = None
+
+            strat = None
+            if result['strat'] is not None:
+                strat = StratigraphicObject.init_stratigraphy(self.session, result['strat'], result['age'], False)
+            point = GeoPoint(strat, result['altitude'] is not None, result['easting'], result['northing'],
+                             result['altitude'], self.session, result['point_set'], result['comment'])
+            point.save_to_db()
+
+        point_file.close()
 
 
 if __name__ == '__main__':
@@ -48,7 +94,7 @@ if __name__ == '__main__':
     """
     if len(sys.argv) < 3:
         raise ArgumentError(
-            "Too less arguments. You need at least the method name and the connection / path of a database!")
+                "Too less arguments. You need at least the method name and the connection / path of a database!")
 
     method = sys.argv[1]
     db = os.path.normpath(sys.argv[2])
